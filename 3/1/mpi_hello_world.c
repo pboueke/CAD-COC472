bdff7151650204e9da159bf4484206ce38ae7608
Pedro Boueke <phboueke@gmail.com>
2016-07-24 21:02:18 -0300

    3.1 fix

--- 3/1/mpi_hello_world.c
+++ 3/1/mpi_hello_world.c
@@ -10,6 +10,11 @@ int main(int argc, char** argv) {
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
+  if (world_rank == 0)
+  {
+    printf("Hello world from the master, the total number of processes is: %d\n", world_size);
+  }
+
   char processor_name[MPI_MAX_PROCESSOR_NAME];
   int name_len;
   MPI_Get_processor_name(processor_name, &name_len);
